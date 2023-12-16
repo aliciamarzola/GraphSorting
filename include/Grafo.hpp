155ecc9717ca9a9223ce31dc61ea06f5d7c51801
aliciachaves <103082430+aliciachaves@users.noreply.github.com>
2023-12-16 13:00:39 -0300

    Create Grafo.hpp

---
+++ include/Grafo.hpp
@@ -0,0 +1,21 @@
+#ifndef GRAFO_H
+#define GRAF0_H
+
+#include "Node.hpp"
+
+class Grafo{
+private:
+    int tamanho;
+    No* vertices;
+public:
+
+    Grafo(int t);
+    int GetTamanho();
+    void addNo(No no);
+    bool found(int* vizinhos, int len, int x, int cores[]);
+    char isGreedy(int cores[]);
+
+};
+
+
+#endif
